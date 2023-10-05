d6126a26e33cc04e3e4518873b551e3379cd3ddb
Zeafax <fred.sanden@gmail.com>
2023-10-05 10:25:32 +0200

    Added images and updated gravity curve calcualtions

--- cart_sim/wagon.py
+++ cart_sim/wagon.py
@@ -2,7 +2,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 
-# Define statespace function
 def f(t, u, m_s, m_v, l, a, f0,g):
     i_rv = 0.01
     i_b = 1/12*m_s*(l**2)+m_s*((l/2-a)**2)
@@ -16,7 +15,7 @@ def f(t, u, m_s, m_v, l, a, f0,g):
     q5 = np.sin(u[1])
     q6 = i_rv+i_b+u[0]**2*m_v
     phi2 = (q1+q2-q3-(q4)/q5)/(q6)
-    dudt = [u[2], u[3], r2,phi2] # [r1,phi1,r2,phi2]
+    dudt = [u[2], u[3], r2,phi2] # [r',phi',r'',phi'']
     return dudt
 
 
@@ -27,7 +26,7 @@ l = 1
 m_s = 5
 m_v = 1
 f0 = 200
-tspan = np.linspace(0, 1.4, 1000)
+tspan = np.linspace(0, 1.5, 100000)
 #u0 = [0, 0, 0.3, 3, 3, 0]
 u0 = [0.3*l,-np.pi/32,0,0] #[r,phi,r1,phi1]
 
@@ -38,9 +37,9 @@ off_beam.direction = -1
 
 # Solve differential equation
 
+f_calc =  f0*2
 
-
-sol = solve_ivp(lambda t, u: f(t, u, m_s, m_v, l, a, 2*f0, g),
+sol = solve_ivp(lambda t, u: f(t, u, m_s, m_v, l, a, f_calc, g),
                 [tspan[0], tspan[-1]], u0, t_eval=tspan, rtol=1e-5, events=off_beam )
 # Plot states
 plt.figure(1)
@@ -72,20 +71,39 @@ last_phi = sol.y[1][-1]
 
 last_r_v = sol.y[2][-1]
 
-print(last_r,last_phi,last_r_v)
-u1 = [last_r*np.cos(last_phi),last_r*np.sin(last_phi),last_r_v*np.cos(last_phi),last_r_v*np.sin(last_phi)] #[x,y,x',y']
-print(u1)
+last_phi_v = sol.y[3][-1]
+
+#print(last_r,last_phi,last_r_v)
+
+print("Values for f0: ",f_calc)
+print("Last time: ",sol.t[-1])
+print("Last r: ",last_r)
+print("last phi: ",last_phi,"\tdegrees: ",last_phi*180/np.pi)
+print("last r velocity: ",last_r_v)
+
+#[x,y,x',y']
+u1 = [
+   last_r*np.cos(last_phi),
+   last_r*np.sin(last_phi),
+   last_r_v*np.cos(last_phi)-last_r*last_phi_v*np.sin(last_phi),
+   last_r_v*np.sin(last_phi)+last_r*last_phi_v*np.cos(last_phi)
+   ] 
+
+
+#print(u1)
 
 
 sol2 = solve_ivp(lambda t, u: f2(t, u),
-                [tspan2[0], tspan2[-1]], u1, t_eval=tspan2, rtol=1e-5, events=hit_ground )
+                [tspan2[0], tspan2[-1]], u1,
+                t_eval=tspan2, rtol=1e-5, events=hit_ground )
 
 
 # plot trajectory
 plt.figure(2)
 plt.plot(sol2.y[0], sol2.y[1])
 xLabel = 'x '+str(sol2.y[0][-1])
-print(xLabel)
+#print(xLabel)
+print("cross x-axis at:",sol2.y[0][-1])
 plt.xlabel(xLabel)
 plt.ylabel('y')
 plt.show(block=True)
